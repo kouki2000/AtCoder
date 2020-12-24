bf9b416029b87cbd74b61678a6478d57dae1ad54
kouki2000 <kouki-tus@outlook.jp>
2020-12-24 14:01:17 +0900

    five

--- problems/recommend/ForbidenList.cpp
+++ problems/recommend/ForbidenList.cpp
@@ -2,56 +2,28 @@
 #define rep(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
 using ll = long long;
+using P = pair<int, int>;
 int INF = 1001001001;
 
 int main()
 {
     int x, n;
     cin >> x >> n;
-    vector<int> p(n);
-    int up = x;
-    int down = x;
-    int up_cnt = 0;
-    int down_cnt = 0;
-    if (n == 0)
+    vector<int> d(102);
+    rep(i, n)
     {
-        cout << x << endl;
+        int p;
+        cin >> p;
+        d[p] = 1;
     }
-    else
+    P ans(99999, -1);
+    for (int i = 0; i <= 101; i++)
     {
-        rep(i, n)
-        {
-            cin >> p[i];
-        }
-        for (int i = 1; i < n; i++)
-        {
-            down -= 1;
-            up += 1;
-            rep(j, n)
-            {
-                if (p[j] == down)
-                {
-                    down_cnt = 1;
-                }
-                if (p[j] == up)
-                {
-                    up_cnt = 1;
-                }
-            }
-            if (down_cnt < 1)
-            {
-                cout << down << endl;
-                return 0;
-            }
-            if (up_cnt < 1)
-            {
-                cout << up << endl;
-                return 0;
-            }
-            down_cnt = 0;
-            up_cnt = 0;
-        }
+        if (d[i] == 1)
+            continue;
+        int dif = abs(x - i);
+        ans = min(ans, P(dif, i));
     }
-
+    cout << ans.second << endl;
     return 0;
 }
