d7c78b64fdf6f888e2c4dc3288e44a15a4f1707f
kouki2000 <kouki-tus@outlook.jp>
2020-12-28 00:13:45 +0900

    nine

---
+++ problems/recommend/FoodsKivedbyEveryone.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+int INF = 1001001001;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> k(n);
+    int a[100][100];
+    int search[100];
+    rep(i, n)
+    {
+        cin >> k[i];
+        int cnt = k[i];
+        rep(j, cnt)
+        {
+            cin >> a[i][j];
+            rep(l, cnt)
+            {
+            }
+        }
+    }
+
+    return 0;
+}
